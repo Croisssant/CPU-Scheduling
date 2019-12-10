2b1ada4fe58f4abcdd4aa97d39856785c711ec30
Choo Wei Ken <weikenchoo@gmail.com>
2019-12-10 22:56:26 +0800

    Added 2 sets of workload

--- CPU_Scheduling/CPU_Scheduling.c
+++ CPU_Scheduling/CPU_Scheduling.c
@@ -8,12 +8,14 @@ int main()
 	// define number of processes
 	int processes[] = {1, 2, 3, 4, 5}; 
 	int n = sizeof processes / sizeof processes[0]; 
-	
+
 	// burst time of all processes 
-	int burstTime[] = {4, 13, 20, 29, 42};
+	int burstTime1[] = {25, 2, 4, 6, 5};
+	int burstTime2[] = {6, 8, 4, 10, 12};
 
 	// arrival Time 
-	int arrivalTime[] = {0, 0, 0, 0, 0};
+	int arrivalTime1[] = {0, 2, 4, 6, 8};
+	int arrivalTime2[] = {0, 0, 0, 0, 0};
 	
 	int choice, timeQuantum;
 	printf("Select the CPU Scheduling Algorithm you desire:\n");
@@ -26,21 +28,21 @@ int main()
 	
 	switch (choice){
 		case 1:
-			FCFS(processes, n, burstTime, arrivalTime); 
+			FCFS(processes, n, burstTime1, arrivalTime1); 
 			break;
 		
 		case 2:
 			printf("Please insert the Time Quantum\n");
 			printf(">> ");
 			scanf("%d", &timeQuantum);
-			RR(n, burstTime, arrivalTime,timeQuantum);
+			RR(n, burstTime1, arrivalTime1,timeQuantum);
 			break;
 		
 		case 3:
 			printf("Please insert the Time Quantum\n");
 			printf(">> ");
 			scanf("%d", &timeQuantum);
-			MLFQ(arrivalTime, burstTime, n, timeQuantum);
+			MLFQ(arrivalTime1, burstTime1, n, timeQuantum);
 			break;
 
 		default:
